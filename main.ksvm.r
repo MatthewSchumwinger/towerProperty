939a2fe1a5eb36a70395fd29965952f194c53294
Matthew Schumwinger <milwaukeestat@gmail.com>
2014-11-26 01:22:52 -0600

    updated ksvm model
    fixed error and added hyper parameters

--- main.ksvm.r
+++ main.ksvm.r
@@ -1,7 +1,7 @@
 
 # path to kaggle data and other files
 setwd("C:\\Users\\milewows\\Documents\\towerProperty")
-#setwd("~/Documents/towerProperty") # Matt's wd path
+setwd("~/Documents/towerProperty") # Matt's wd path
 
 source("config.r")
 source("helpers.r")
@@ -20,14 +20,20 @@ allData = prepareSplits(rawData, allPredictors, c(0))
 
 filter = "199|200|2010|price.level|add_no|TELEMAN|JOHANN|ROSSINI|conc_missed|add_price|add_tickets|add_tickets_seats|section_2013_2014|multiple.subs|billing.city|is.us|relationship|outside|City|State|Lat|Long|package|section|location|geo|hotspot" 
 useLogTransform = FALSE 
-kernel="rbfdot"
-sigma=0.001
+kernel="besseldot"
+sigma=0.01
 C=100
 epsilon=0.005
 tol = 0.05
 cross = 5
 numfolds = 10
 clean = T
+type = NULL
+degree = 1
+scale = 1
+offset = 0
+order = 20
+kpar =  list(degree=degree, order=order, sigma=sigma) #"automatic" #list(sigma=sigma)
 
 rawData = readData(useLogTransform)
 
@@ -39,10 +45,13 @@ folds = sample(1:numfolds, nrow(allData$allSet), replace=T)
 
 predictors = preparePredictors(rawData, filter)
 
+
+##---------
 trainError = 0
 testError = 0
 testErrorInact = 0
 testErrorVar = 0
+
 for(i in 1:numfolds) {
   
   data = prepareSplits(rawData, predictors, which(folds == i))
@@ -51,15 +60,15 @@ for(i in 1:numfolds) {
   }
   
   print(paste("Start ksvm fold ", i))
-  ksvm.fit = ksvm(total~.,data=data$trainSet, scaled=T, kernel=kernel, kpar=list(sigma=sigma), 
-                  C=C, epsilon=epsilon, tol=tol, cross=cross)
+  ksvm.fit = ksvm(total~.,data=data$trainSet, scaled=T, kernel=kernel, kpar=kpar, 
+                  C=C, epsilon=epsilon, tol=tol, cross=cross, type=type)
   ksvm.pred = predict(ksvm.fit, newdata=data$testSet)
   ksvm.train = predict(ksvm.fit, newdata=data$trainSet)
   print("Train prediction")
   trainError = trainError + evaluateModel(ksvm.train, data$trainSet$total, useLogTransform)
   print("Raw prediction")
   testError = testError + evaluateModel(ksvm.pred, data$testAnswers, useLogTransform)
-
+  
   
   print("Adjusting for inactive")
   adjusted = adjustPredictionsInactive(ksvm.pred, data.frame("account.id"=data$testAccounts), allData$predictors)
@@ -78,12 +87,12 @@ print(paste("Final test error with inactive adj=", testErrorInact / tries, " bas
 print(paste("Final test error with no variance adj =", testErrorVar / tries, " based on ", tries, " tries"))
 
 
-ksvm.fit = ksvm(total~.,data=data$trainSet, scaled=T, kernel=kernel, kpar=list(sigma=sigma), 
-                C=C, epsilon=epsilon, tol=tol, cross=cross)
+ksvm.fit = ksvm(total~.,data=data$trainSet, scaled=T, kernel=kernel, kpar=kpar,
+                C=C, epsilon=epsilon, tol=tol, cross=cross, type=type)
 
 predictSet = prepareDataToPredict(data$predictors)
 predictSetAll = prepareDataToPredict(allData$predictors)
-predictions = predict(ksvm.fit, newdata=predictSet$testSet, n.trees=trees)
+predictions = predict(ksvm.fit, newdata=predictSet$testSet)#, n.trees=trees)
 
 predictions = adjustPredictionsInactive(predictions, data.frame("account.id"=predictSet$accounts), 
                                         predictSetAll$testSetAll)
@@ -92,4 +101,5 @@ if(useLogTransform) {
   predictions = exp(predictions)-1
 }
 
-dumpResponse("ML_svm_sub", predictSet$accounts, predictions)
+dumpResponse("MS_svm_besseldot_sub", predictSet$accounts, predictions)
+
